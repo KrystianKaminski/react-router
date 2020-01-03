531a968c1e1e6468edcc28cb5219a28e23248be4
KrystianKaminski <krystiankaminski07@gmail.com>
2020-01-03 14:16:04 +0100

    Use RegExp with Routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 const App = (props) => (
   <Router>
   <div>
-    <Route path='/:page?-:subpage?' render={({match}) => (
+    <Route path='/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)' render={({match}) => (
       <h1>
-        PAGE: {match.params.page || 'Home'}<br/>
-        SUBPAGE: {match.params.subpage}
+        paramA: {match.params.a}<br/>
+        paramB: {match.params.b}
       </h1>
     )} />
   </div>
