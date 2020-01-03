b413359ada64e7d397fcab1aca9222c3be1d1c25
KrystianKaminski <krystiankaminski07@gmail.com>
2020-01-03 15:02:24 +0100

    Intercept route changes with Prompt component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Prompt} from 'react-router-dom'
 
 import './App.css'
 
-const Links = () => 
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/old">Old</Link>
-    <Link to="/new">New</Link>
-  </nav>
+const Home = () => (<h1>Home</h1>)
+
+class Form extends React.Component {
+  state = { dirty: false }
+  setDirty = () => this.setState({dirty: true})
+
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+        <input type="text" onInput={this.setDirty} />
+        <Prompt
+          when={this.state.dirty}
+          message="Data will be lost!"
+        />
+      </div>
+    )
+  }
+}
 
 const App = (props) => (
   <Router>
   <div>
-    <Links/>
-      <Route exact path="/" render={() => (<h1>Home</h1>)} />
-      <Route path="/new" render={() => (<h1>New</h1>)} />
-      <Route path="/old" render={() => <Redirect to="/new"/>}/>
+    <Link to="/">Home</Link>
+    <Link to="/form">Form</Link>
+    <Route exact path="/" component={Home}/>
+    <Route path="/form" component={Form}/>
   </div>
   </Router>
 )
